32bc18d64acaceccb0440a5a20184bbd333f58de
synapse <ebonfortress@gmail.com>
2010-08-25 11:55:21 +0400

    Initial

---
+++ DbHelper.Tests/SqlDbQueryTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DbHelper.Tests
+{
+	/// <summary>
+	/// Summary description for UnitTest1
+	/// </summary>
+	[TestClass]
+	public class SqlDbQueryTest
+	{
+		public SqlDbQueryTest()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+
+		private const string ConnStr = @"Data Source=localhost\sqlexpress;Initial Catalog=SandBox;Integrated Security=True";
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Use TestInitialize to run code before running each test 
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
+
+		[TestMethod]
+		public void Returns_Results() {
+			var c = SqlConnectionFactory.Create(ConnStr);
+			
+			IEnumerable<IDictionary<string, object>> result = null;
+
+			c.Query(q => {
+			        	result = q.Select("select Name from [Full]");
+			        });
+
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.Any());
+		}
+
+		[TestMethod]
+		public void SelectExpando_Returns_Results() {
+			var c = SqlConnectionFactory.Create(ConnStr);
+
+			IEnumerable<dynamic> result = null;
+
+			c.Query(q => {
+			        	result = q.SelectExpando("select Name from [Full]");
+			        });
+
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.Any());
+
+			dynamic row = result.FirstOrDefault();
+
+			Assert.IsNotNull(row);
+			Assert.IsNotNull(row.Name);
+
+			Console.WriteLine(row.Name);
+		}
+	}
+}
---
+++ DbHelper/SqlDbConnection.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DbHelper {
+	public static class Extensions {
+		public static void Each<T>(this IEnumerable<T> source, Action<T> action) {
+			Each(source, (i, v) => action(v));
+		}
+		public static void Each<T>(this IEnumerable<T> source, Action<int, T> action) {
+			var e = source.GetEnumerator();
+			for (int i = 0; e.MoveNext(); i++) {
+				action(i, e.Current);
+			}
+		}
+
+	}
+
+	public interface IDbQuery {
+		IEnumerable<IDictionary<string, object>> Select(string sql);
+		IEnumerable<T> Select<T>(string sql, Func<IDictionary<string, object>, T> objectGetter);
+		IEnumerable<dynamic> SelectExpando(string sql);
+		void Insert(string table, object values);
+	}
+
+	public interface IDbConnection {
+		void Query(Action<IDbQuery> action);
+	}
+
+	public class SqlConnectionFactory {
+		public static IDbConnection Create(string connString) {
+			return new SqlDbConnection(connString);
+		}
+
+		private class SqlDbConnection : IDbConnection {
+			private readonly string _connString;
+
+			public SqlDbConnection(string connString) {
+				_connString = connString;
+			}
+
+			public void Query(Action<IDbQuery> action) {
+				using (var conn = new SqlConnection(_connString)) {
+					conn.Open();
+					var q = new SqlDbQuery(conn);
+					action(q);
+				}
+			}
+		}
+
+		private class SqlDbQuery : IDbQuery {
+			private readonly SqlConnection _conn;
+
+			public SqlDbQuery(SqlConnection conn) {
+				_conn = conn;
+			}
+
+			public IEnumerable<IDictionary<string, object>> Select(string sql) {
+				using (var cmd = new SqlCommand(sql, _conn)) {
+					using (var rdr = cmd.ExecuteReader()) {
+						var fields = new string[rdr.FieldCount];
+						for (int i = 0; i < fields.Length; i++) {
+							fields[i] = rdr.GetName(i);
+						}
+						var result = new List<IDictionary<string, object>>();
+						while (rdr.Read()) {
+							var row = new Dictionary<string, object>();
+							fields.Each((i, v) => row[v] = rdr.GetValue(i));
+							result.Add(row);
+						}
+						return result;
+					}
+				}
+			}
+
+			public IEnumerable<T> Select<T>(string sql, Func<IDictionary<string, object>, T> objectGetter) {
+				return Select(sql).Select(objectGetter);
+			}
+
+			public IEnumerable<dynamic> SelectExpando(string sql) {
+				return Select(sql).Select(row => {
+				                          	var e = new ExpandoObject();
+				                          	row.Each(field => ((IDictionary<string, object>) e)[field.Key] = field.Value);
+				                          	return e;
+				                          });
+
+                }
+
+			public void Insert(string table, object values) {
+
+				using (var cmd = new SqlCommand()) {
+					cmd.Connection = _conn;
+					cmd.Parameters.AddRange(values.GetType().GetFields().Select(fi => new SqlParameter(fi.Name, fi.GetValue(values))).ToArray());
+					string text = "insert into " + table;
+					text += String.Join(",", values.GetType().GetFields(BindingFlags.Public).Select(fi => fi.Name));
+				}
+			}
+		}
+	}
+}
